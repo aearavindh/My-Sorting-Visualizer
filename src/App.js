b539ca91057dde181c53517dd137db39682fb503
aearavindh <aearavindh@gmail.com>
2022-03-03 22:48:48 +0530

    CSS adjustments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Line from "./Line";
 
 function App() {
   const [elements, setElements] = useState([]);
-  const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [generateButton, setGenerateButton] = useState(false);
   const [lastChangedIndex, setLastChangedIndex] = useState(null);
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 600px)").matches
+  );
+  let [numberOfLines, setNumberOfLines] = useState(50);
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 600px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
+  useEffect(() => {
+    if (matches) {
+      setNumberOfLines(50);
+    } else {
+      setNumberOfLines(25);
+    }
+  }, [matches]);
 
   const generateRandomArray = () => {
     setLastChangedIndex(null);
     let arr = [];
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < numberOfLines; i++) {
       arr[i] = Math.floor((Math.random() * 100) % 25);
     }
     setElements(arr);
+    setDisabled(false);
   };
 
   const sleep = (seconds) => {
@@ -25,6 +45,7 @@ function App() {
 
   const bubbleSortArray = () => {
     setDisabled(true);
+    setGenerateButton(true);
     let copy = elements;
     for (let i = 0; i < copy.length - 1; i++) {
       for (let j = 0; j < copy.length - 1 - i; j++) {
@@ -37,8 +58,9 @@ function App() {
             setElements([...copy]);
           }
           sleep(0.01);
-          if (i === 48) {
+          if (i === numberOfLines - 2) {
             setDisabled(false);
+            setGenerateButton(false);
           }
         }, 500);
       }
@@ -49,6 +71,7 @@ function App() {
 
   const insertionSortArray = () => {
     setDisabled(true);
+    setGenerateButton(true);
     let copy = elements;
     for (let i = 1; i < copy.length; i++) {
       setTimeout(() => {
@@ -65,8 +88,9 @@ function App() {
         setLastChangedIndex(j + 1);
         setElements([...copy]);
         sleep(0.2);
-        if (i === 49) {
+        if (i === numberOfLines - 1) {
           setDisabled(false);
+          setGenerateButton(false);
         }
       }, 500);
     }
@@ -76,6 +100,7 @@ function App() {
 
   const selectionSortArray = async () => {
     setDisabled(true);
+    setGenerateButton(true);
     let copy = elements;
     for (let i = 0; i < copy.length - 1; i++) {
       setTimeout(() => {
@@ -90,8 +115,9 @@ function App() {
         setLastChangedIndex(i);
         setElements([...copy]);
         sleep(0.2);
-        if (i === 48) {
+        if (i === numberOfLines - 2) {
           setDisabled(false);
+          setGenerateButton(false);
         }
       }, 500);
     }
@@ -102,7 +128,7 @@ function App() {
   return (
     <div className="app">
       <header className="app_header">
-        <button disabled={disabled} onClick={generateRandomArray}>
+        <button disabled={generateButton} onClick={generateRandomArray}>
           Generate Random Array
         </button>
         <button disabled={disabled} onClick={bubbleSortArray}>
@@ -119,7 +145,10 @@ function App() {
       </header>
       <div className="app_body">
         {elements.length === 0 ? (
-          <h1>Welcome to Sorting Visualizer</h1>
+          <h3 className="app_body--placeholder">
+            Welcome to Sorting Visualizer{" "}
+            {matches ? "" : "(Please view in computer for better experience)"}
+          </h3>
         ) : (
           elements.map((a, index) => {
             return (
